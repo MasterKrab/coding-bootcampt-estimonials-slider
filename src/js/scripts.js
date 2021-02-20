6cce5845be866827daff06e7efe0401c531ee692
Carlos Díaz <57598956+cqrlosArch@users.noreply.github.com>
2021-02-20 20:44:32 +0100

    Update scripts.js
    Use tag <tamplate> html

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,25 @@
 const tesminonialElement = document.getElementById("testimonial-text")
 const sliderControl = document.getElementById("slider")
 const sliderImg = document.getElementById("slider-img")
+const template = document.getElementById('template').content;
 
 const changeSlider = type =>{
-   tesminonialElement.innerHTML = `
-   <blockquote class="testimonial__quote">“${persons[type].testimonial}”</blockquote>
-        <p class="testimonial__author">
-          <cite class="testimonial__name">${persons[type].name}</cite> ${persons[type].profession}
-        </p>
-   `
+   tesminonialElement.innerHTML = ''
+   
+   template.querySelector('.testimonial__quote').textContent = persons[type].testimonial;
+    
+   const author = template.querySelector('.testimonial__author');
+   const name = template.querySelector('.testimonial__name');
+
+   name.textContent = persons[type].name;
+   author.textContent = persons[type].profession;
+
+   author.insertAdjacentElement('afterbegin', name);
+
+   const node = document.importNode(template, true);
+
+   tesminonialElement.appendChild(node);
+  
    sliderImg.src= `../assets/img/${persons[type].img}`
    sliderImg.alt= `${persons[type].name} profile photo`
 }
